09b3077e5aa5617df4940873acf3b78a930ba29e
Charles Chen <779970+CharlieDigital@users.noreply.github.com>
2026-06-06 19:13:36 -0400

    Fix host configuration; add Firebase container

--- host/AppHost.cs
+++ host/AppHost.cs
@@ -1,3 +1,5 @@
+using Aspire.Hosting.Yarp;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 // The main backend API runtime.
@@ -13,6 +15,17 @@ var buildGenerate = builder
     )
     .WithEnvironment("GEN", "true");
 
+// Add glider MCP server (Codex does not like the CLI app because of sandbox...)
+// See: https://glidermcp.com/
+var gliderMcp = builder
+    .AddExecutable("glider-mcp", "glider", "./", "--transport", "http", "--port", "5055")
+    .WithHttpEndpoint(5051, 5055, name: "http", isProxied: true);
+
+// Add Firebase container for auth
+var firebase = builder
+    .AddDockerfile(name: "firebase-emulator", "Dockerfile.firebase")
+    .WithHttpEndpoint(9099, 9099, name: "firebase", isProxied: true);
+
 // Vue front-end app.
 var frontend = builder.AddViteApp(name: "tasklet-ui", appDirectory: "../src/web").WithYarn();
 
@@ -23,10 +36,10 @@ var proxy = builder
     .WithConfiguration(yarp =>
     {
         // Proxy route to the backend as: http://api.localhost:8089
-        yarp.AddRoute(backend).WithMatHosts("api.localhost");
+        yarp.AddRoute(backend).WithMatchHosts("api.localhost");
 
         // Proxy route to the frontend as: http://tasklet.localhost:8089
-        yarp.AddRoute(frontend).WithMatHosts("tasklet.localhost");
+        yarp.AddRoute(frontend).WithMatchHosts("tasklet.localhost");
     });
 
 builder.Build().Run();
